2032362612d38855d932646c66d23d027fad7955
Philipp Weber <phlipse@phwe.de>
2017-08-21 17:48:05 +0200

    Add test cases for endpoints

---
+++ endpoints_test.go
@@ -0,0 +1,67 @@
+// Copyright 2017 (c) Philipp Weber. All rights reserved.
+// Use of this source code is governed by the MIT license
+// which can be found in the repositorys LICENSE file.
+
+package hibpgo
+
+import "testing"
+
+func TestBreachedAccount(t *testing.T) {
+	b, err := BreachedAccount("test@example.com", "", false, false)
+	if err != nil {
+		t.Errorf("got error: %s", err.Error())
+	}
+	if len(b) == 0 {
+		t.Errorf("expected breaches got none")
+	}
+}
+
+func TestBreaches(t *testing.T) {
+	b, err := Breaches("adobe.com")
+	if err != nil {
+		t.Errorf("got error: %s", err.Error())
+	}
+	if len(b) == 0 {
+		t.Errorf("expected breaches got none")
+	}
+}
+
+func TestBreach(t *testing.T) {
+	b, err := Breach("Adobe")
+	if err != nil {
+		t.Errorf("got error: %s", err.Error())
+	}
+	if b.Name == "" {
+		t.Errorf("expected breach got none")
+	}
+}
+
+func TestDataclasses(t *testing.T) {
+	d, err := Dataclasses()
+	if err != nil {
+		t.Errorf("got error: %s", err.Error())
+	}
+	if len(d) == 0 {
+		t.Errorf("expected dataclasses got none")
+	}
+}
+
+func TestPasteAccount(t *testing.T) {
+	p, err := PasteAccount("test@example.com")
+	if err != nil {
+		t.Errorf("got error: %s", err.Error())
+	}
+	if len(p) == 0 {
+		t.Errorf("expected pastes got none")
+	}
+}
+
+func TestPwnedPassword(t *testing.T) {
+	p, err := PwnedPassword("12 34", false)
+	if err != nil {
+		t.Errorf("got error: %s", err.Error())
+	}
+	if !p {
+		t.Errorf("expected true got %t", p)
+	}
+}
